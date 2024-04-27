2b0cddeffbb5eece9154af7e2fbaada8d97706ea
gtorres64 <george.torres641@gmail.com>
2024-04-27 19:13:39 +0000

    added sql js

---
+++ sql/mysql-config.js
@@ -0,0 +1,61 @@
+const mysql = require('mysql2');
+require('dotenv').config()
+
+const pool = mysql.createPool({
+    host: process.env.SQL_HOST,
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASS,
+    database: process.env.SQL_DB
+}).promise()
+
+async function getUsers() {
+    const [rows] = await pool.query("SELECT * FROM users");
+    return rows;
+}
+
+async function insertUser(username, password, email, firstName, lastName, uId) {
+    const [rows] = await pool.query(`
+    INSERT INTO users (
+    username, 
+    password, 
+    email, 
+    firstName, 
+    lastName,
+    uId) 
+    VALUES (?, ?, ?, ?, ?, ?)`,
+    [username, password, email, firstName, lastName, uId]);
+    return rows.insertId;
+}
+
+async function findUserByEmail(email) {
+    const [rows] = await pool.query(`
+    SELECT * FROM users WHERE email = ?`,
+    [email]);
+    console.log(rows);
+    return rows[0];
+}
+
+async function findUserById(userID) {
+    const [rows] = await pool.query(`
+    SELECT * FROM users WHERE userID = ?`,
+    [userID]);
+    console.log(rows);
+    return rows[0].userID;
+}
+
+(async () => {
+    try {
+        const dbUsers = await getUsers();
+        //console.log(dbUsers);
+    } catch (error) {
+        console.error("ERROR GETTING USERS", error);
+    }
+})();
+
+
+module.exports = {
+    pool,
+    insertUser,
+    findUserByEmail,
+    findUserById
+};
